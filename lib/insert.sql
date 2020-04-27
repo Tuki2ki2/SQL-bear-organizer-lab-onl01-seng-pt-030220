30ef6905e7a2255304fef09736695575bd616eb6
Erik Prenzlin <eprenzlin@gmail.com>
2020-04-27 20:05:06 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Mr. Chocolate', 23, 'Male', 'Brown', 'Hungry', 'yes');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Rowdy', 20, 'Female', 'Polar', 'Sleepy', 'no');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Tabitha', 12, 'Male', 'Grizzly', 'Angry', 'yes');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Sergeant Brown', 3, 'Female', 'Lazy', 'Hungry', 'no');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Melissa', 6, 'Male', 'Sun', 'Happy', 'yes');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ( 'Grinch', 210, 'Female', 'Bored', 'Hungry', 'no');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ('Wendy', 20, 'Male', 'Black', 'Hungry', 'yes');
+INSERT INTO bears ('name', 'age', 'gender', 'color', 'temperament', 'alive') VALUES ( Null, 10, 'Female', 'Polar', 'Lazy', 'yes')
